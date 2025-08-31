9f8b305bc8d5c1895704c8bd36442151fedce359
daiki oikawa <daiki404@icloud.com>
2025-08-31 16:39:14 +0900

    仮想スクロール実装バグもある

--- app/Table/MatrixTable.tsx
+++ app/Table/MatrixTable.tsx
@@ -1,3 +1,4 @@
+import React from 'react';
 import Table from '@mui/material/Table';
 import TableBody from '@mui/material/TableBody';
 import TableCell from '@mui/material/TableCell';
@@ -7,6 +8,7 @@ import TableRow from '@mui/material/TableRow';
 import Paper from '@mui/material/Paper';
 import { useEffect, useState } from 'react';
 import { ChcekboxCell } from '~/CheckboxCell/CheckboxCell';
+import { TableVirtuoso } from 'react-virtuoso';
 
 const createColumns = (count: number) =>
   Array.from({ length: count }, (_, i) => `column ${i + 1}`);
@@ -29,39 +31,61 @@ export const MatrixTable = () => {
     }));
   };
 
+  const VirtuosoTableComponents = {
+    Scroller: React.forwardRef<HTMLDivElement>((props, ref) => (
+      <TableContainer component={Paper} {...props} ref={ref} />
+    )),
+    Table: (props: any) => (
+      <Table {...props} sx={{ minWidth: 650 }} size="small" />
+    ),
+    TableHead: React.forwardRef<HTMLTableSectionElement>((props, ref) => (
+      <TableHead {...props} ref={ref} />
+    )),
+    TableRow,
+    TableBody: React.forwardRef<HTMLTableSectionElement>((props, ref) => (
+      <TableBody {...props} ref={ref} />
+    )),
+  };
+
+  const FixedHeaderContent = () => {
+    return (
+      <TableRow>
+        <TableCell></TableCell>
+        {columns.map((col, index) => (
+          <TableCell key={index}>{col}</TableCell>
+        ))}
+      </TableRow>
+    );
+  };
+
+  const RowContent = (_index: number, row: string) => {
+    return (
+      <TableRow key={row}>
+        <TableCell>{row}</TableCell>
+        {columns.map((col) => (
+          <ChcekboxCell
+            key={`${row}_${col}`}
+            selectedKey={`${row}_${col}`}
+            selected={!!selected[`${row}_${col}`]}
+            setSelected={onChange}
+          />
+        ))}
+      </TableRow>
+    );
+  };
+
   useEffect(() => {
     console.log(selected);
   }, [selected]);
 
   return (
-    <TableContainer component={Paper}>
-      <Table sx={{ minWidth: 650 }} size="small">
-        <TableHead>
-          <TableRow>
-            <TableCell></TableCell>
-            {columns.map((col, index) => (
-              <TableCell key={index}>{col}</TableCell>
-            ))}
-          </TableRow>
-        </TableHead>
-        <TableBody>
-          {rows.map((row) => {
-            return (
-              <TableRow key={row}>
-                <TableCell>{row}</TableCell>
-                {columns.map((col) => (
-                  <ChcekboxCell
-                    key={`${row}_${col}`}
-                    selectedKey={`${row}_${col}`}
-                    selected={!!selected[`${row}_${col}`]}
-                    setSelected={onChange}
-                  />
-                ))}
-              </TableRow>
-            );
-          })}
-        </TableBody>
-      </Table>
-    </TableContainer>
+    <Paper style={{ height: '100vh', width: '100%' }}>
+      <TableVirtuoso
+        data={rows}
+        components={VirtuosoTableComponents}
+        fixedHeaderContent={FixedHeaderContent}
+        itemContent={RowContent}
+      />
+    </Paper>
   );
 };
